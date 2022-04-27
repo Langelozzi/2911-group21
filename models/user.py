bce1c3212c45751c9d860e42091bc6004d90c768
Langelozzi <lucasangelozzi32@gmail.com>
2022-04-26 19:53:25 -0700

    created a few mock json documents as well as bare bones classes for a user and a review object

--- models/user.py
+++ models/user.py
@@ -3,4 +3,14 @@
 '''A class to represent a single user'''
 
 class User:
-    pass
+    def __init__(self, full_name: str, email: str, password: str) -> None:
+        self.name = full_name
+        self.email = email
+        self.password = password
+
+    def change_password(self, old_pass, new_pass) -> bool:
+        if old_pass == self.password:
+            self.password = new_pass
+            return True
+        else:
+            return False
