e36a8785482c056565b5c606e3cc64514aec70ac
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-04-27 12:13:06 -0700

    added comments and docstrings

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
+# Web app
+
+'''File containing endpoints for our Course Rating System application'''
+
+# Imports
 from flask import Flask, render_template, request, jsonify
 from models.review import Review
 from models.user import User
 from models.review_collection import ReviewCollection
 
+# Creating the app object from flask
 app = Flask(__name__)
 
-#Create a flask app based on the templates in the templates folder
-@app.route("/")
+#Create a flask endpoint that renders the html homepage
+@app.route("/", methods=["GET"])
 def homepage():
     collection = ReviewCollection()
     reviews = collection.reviews
     #Render the homepage html all the reviews in divs as plain text
     return render_template("home.html", reviews=reviews), 200
-    
 
 #Return all the reviews as JSON
 @app.route("/api/reviews", methods=["GET"])
@@ -24,5 +29,7 @@ def get_reviews():
     #Convert to JSON
     return jsonify(reviews), 200
 
+# starting app in debug mode if ran
+# debug mode auto restarts the server after every change made to the code
 if __name__ == "__main__":
     app.run(debug=True)
--- models/review.py
+++ models/review.py
@@ -6,12 +6,29 @@
 from models.user import User
 
 class Review:
-    def __init__(self, user: str, title: str, course: str, instructor: str, review: str, rating: int) -> None:
+    '''class to represent a review object'''
+    
+    def __init__(self, user_email: str, title: str, course: str, instructor: str, review: str, rating: int) -> None:
+        """constructor method that sets variables and will do data checking
+
+        Args:
+            user_email (str): the email the user logs in with
+            title (str): the title of the review
+            course (str): what course the review is for
+            instructor (str): who taught the course
+            review (str): the content of the review. the details
+            rating (int): a score out of 5
+
+        Raises:
+            ValueError: if the rating is not a number from 1-5
+        """
+        
+        # checking that the rating is 1-5
         if rating not in (1,2,3,4,5):
             raise ValueError
         # will add more checks in later sprints^
         
-        self.user = user
+        self.user_email = user_email
         self.title = title
         self.course = course
         self.instructor = instructor
@@ -19,8 +36,14 @@ class Review:
         self.rating = rating 
 
     def to_dict(self) -> dict:
+        """Method to transform the review into a dictionary for json purposes
+
+        Returns:
+            dict: the dictionary with the review info
+        """
+        
         return {
-            "User": self.user,
+            "UserEmail": self.user_email,
             "Title": self.title,
             "Course": self.course,
             "Instructor": self.instructor,
