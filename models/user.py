a44150093fbab9a9f94d430e94c556d43a6159de
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-04-30 13:55:38 -0700

    edited the user class and added comments

--- models/user.py
+++ models/user.py
@@ -4,14 +4,23 @@
 
 class User:
     def __init__(self, id: str, full_name: str, email: str, password: str) -> None:
+        """Constructor method for a user object
+
+        Args:
+            id (str): the random generated uuid
+            full_name (str): the first and last name of the user
+            email (str): the users preferred email address
+            password (str): the users encrypted password
+        """
+        
         self.public_id = id
         self.name = full_name
         self.email = email
         self.password = password
 
-    def change_password(self, old_pass, new_pass) -> bool:
-        if old_pass == self.password:
-            self.password = new_pass
-            return True
-        else:
-            return False
+    # def change_password(self, old_pass, new_pass) -> bool:
+    #     if old_pass == self.password:
+    #         self.password = new_pass
+    #         return True
+    #     else:
+    #         return False
