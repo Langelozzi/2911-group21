c2be43db4eaea5ccdf737170456399d216f99412
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-05-04 10:23:11 -0700

    successfully added sorting functionality to the homepage

--- app.py
+++ app.py
@@ -12,10 +12,28 @@ from models.review_collection import ReviewCollection
 app = Flask(__name__)
 
 #Create a flask endpoint that renders the html homepage
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def homepage():
     collection = ReviewCollection()
     reviews = collection.reviews
+    
+    # if a post request is made to this enpoint. This happens when the form gets submitted
+    if request.method == "POST":
+        # extracting the values of the drop down options and the string in the search box
+        search_option = request.form.get("teams")
+        search_string = request.form['search']
+        
+        # line 27 checks if the form submit button has been clicked. sorted = input name, Submit = input value
+        if request.form.get('sorted') == 'Submit':
+            # determining if the chosen sort option was instructor or course number based on the value of the dropdown option
+            if search_option.lower() == 'coursen':
+                # sorting the reviews and returning the homepage with the sorted reviews
+                sorted_reviews = collection.get_review_by_course(search_string)
+                return render_template("home.html", reviews=sorted_reviews), 200
+            elif search_option.lower() == 'instructor':
+                sorted_reviews = collection.get_review_by_instr(search_string)
+                return render_template("home.html", reviews=sorted_reviews), 200
+
     #Render the homepage html all the reviews in divs as plain text
     return render_template("home.html", reviews=reviews), 200
 
@@ -36,10 +54,10 @@ def homepage():
 #         search_string = request.form['search_box']
 #         coll = ReviewCollection()
         
-#         if search_option.lower() == 'course':
-#             sorted_reviews = coll.get_review_by_course(search_string)
-#         elif search_option.lower() == 'instructor':
-#             sorted_reviews = coll.get_review_by_instr(search_string)
+        # if search_option.lower() == 'course':
+        #     sorted_reviews = coll.get_review_by_course(search_string)
+        # elif search_option.lower() == 'instructor':
+        #     sorted_reviews = coll.get_review_by_instr(search_string)
         
 #         return render_template("home.html", reviews=sorted_reviews), 200
 #     except:
