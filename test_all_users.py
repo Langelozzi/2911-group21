02919bee5665f72afa5cbed03577c07c193bc040
Simon <sfreeman15@my.bcit.ca>
2022-05-12 12:47:01 -0700

    Made tests for all_users.py

--- test_all_users.py
+++ test_all_users.py
@@ -10,25 +10,25 @@ dummy_data = ''' [
         "Id": "91e28efe-5fa3-4bea-8eca-76587b20cd1e",
         "Name": "Joe Smith",
         "Email": "jsmith@my.bcit.ca",
-        "Password": "sha256$b7QCMW5vqi96RvQH$071915866d88d949a8416510310b9a7c4966f67e96472b48a02d9d112e1dd3fc"
+        "Password": "Howdy1234!"
     },
     {
         "Id": "8da671ab-fb7f-4cec-a6d4-7d4cb1d0c53e",
         "Name": "Lucas Angelozzi",
         "Email": "langelozzi@my.bcit.ca",
-        "Password": "sha256$tFegpSWp0SlFFIAa$203c91b9d280c32fff4d00224c6975d3e55a386188e9e245b058a43205adacb4"
+        "Password": "Goodbye4321!"
     },
     {
         "Id": "5307a183-31f6-44c8-847c-4a1b3a257885",
         "Name": "John Doe",
         "Email": "jdoe@my.bcit.ca",
-        "Password": "sha256$CiQDhqwJAnDBRHZQ$58df0d5fee3535d8d2600204c8c098bc14f1aaae44821152a8af3406627542a1"
+        "Password": "Itadakimasu2@"
     }
 ] '''
 
 @pytest.fixture
 @patch("builtins.open", new_callable=mock_open, read_data=dummy_data)
-def review_collection(mock_file):
+def all_users(mock_file):
     """This functions is a fixture that returns a mock file object
 
     Args:
@@ -42,6 +42,19 @@ def review_collection(mock_file):
 
 
 def test_length_all_users(all_users):
+    assert len(all_users.users) == 3
     
-    
+def test_get_user_by_name(all_users):
+    correct_user = all_users.get_user_by_name("Joe Smith")
+    assert "Joe Smith" in [usr.name for usr in correct_user]
 
+def test_get_user_by_email(all_users):
+    correct_email = all_users.get_user_by_email("jsmith@my.bcit.ca")
+    assert correct_email.email == "jsmith@my.bcit.ca"
+
+def test_user_by_id(all_users):
+    correct_id = all_users.get_user_by_id("91e28efe-5fa3-4bea-8eca-76587b20cd1e")
+    assert correct_id.public_id == "91e28efe-5fa3-4bea-8eca-76587b20cd1e"
+
+
+    
